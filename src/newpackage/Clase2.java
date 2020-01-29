7509c47baa95d31830df7fc030d8a2e0d8d6f956
dam <dam@172.26.39.75>
2020-01-29 12:12:26 +0100

    clase 2 y rama

---
+++ src/newpackage/Clase2.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase2 {
+    
+    int sumas;
+    String apellidos;
+}
